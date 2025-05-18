ad8073f39f194deeae58237dfc82aff9aa070897
Jione <86013565+Ji-One1@users.noreply.github.com>
2025-05-17 22:49:55 -0400

    Improve set command

--- interpreter.c
+++ interpreter.c
@@ -4,11 +4,16 @@
 #include "shellmemory.h"
 #include "shell.h"
 
-int MAX_ARGS_SIZE = 3;
+int MAX_ARGS_SIZE = 7;
 
 int badcommand() {
     printf("Unknown Command\n");
     return 1;
+}
+
+int badCommandTooManyArgs() {
+    printf("Bad command: Too many arguments\n");
+    return 2;
 }
 
 // For run command only
@@ -19,7 +24,7 @@ int badcommandFileDoesNotExist() {
 
 int help();
 int quit();
-int set(char *var, char *value);
+int set(char *var, char *values[], int args_size);
 int print(char *var);
 int run(char *script);
 int badcommandFileDoesNotExist();
@@ -28,8 +33,10 @@ int badcommandFileDoesNotExist();
 int interpreter(char* command_args[], int args_size) {
     int i;
 
-    if (args_size < 1 || args_size > MAX_ARGS_SIZE) {
+    if (args_size < 1) {
         return badcommand();
+    } else if (args_size > MAX_ARGS_SIZE) {
+        return badCommandTooManyArgs();
     }
 
     // Terminate args at newline chars
@@ -48,9 +55,7 @@ int interpreter(char* command_args[], int args_size) {
         return quit();
 
     } else if (strcmp(command_args[0], "set") == 0) {
-        //set
-        if (args_size != 3) return badcommand();
-        return set(command_args[1], command_args[2]);
+        return set(command_args[1], command_args, args_size);
 
     } else if (strcmp(command_args[0], "print") == 0) {
         if (args_size != 2) return badcommand();
@@ -81,18 +86,16 @@ int quit() {
     exit(0);
 }
 
-int set(char *var, char *value) {
-    char *link = "=";
+int set(char *var, char *values[], int args_size) {
 
-    /* PART 1: You might want to write code that looks something like this.
-         You should look up documentation for strcpy and strcat.
+    char value[MAX_USER_INPUT] = "";
 
-    char buffer[MAX_USER_INPUT];
-    strcpy(buffer, var);
-    strcat(buffer, link);
-    strcat(buffer, value);
-    */
-
+    for (int i = 2; i < args_size; i++) {
+        strcat(value, values[i]);
+        if (i < args_size - 1){
+            strcat(value, " ");
+        }
+    }
     mem_set_value(var, value);
 
     return 0;
